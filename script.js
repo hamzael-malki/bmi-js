a1976d9555ec9cb45906c3ec957748157b8a5249
hamzaelmalki96@gmail.com <hamzaelmalki96@gmail.com>
2023-07-10 09:34:47 +0200

    Mise à jour du fichier script.js avec les validations côté client

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
     $('form').submit(function(event) {
-        event.preventDefault(); // Prevent the default form submission behavior
+        event.preventDefault(); // Empêche le comportement de soumission du formulaire par défaut
 
+        // Récupère les valeurs du poids et de la taille
         var weight = parseFloat($('#weight').val());
         var height = parseFloat($('#height').val());
 
-        // Perform the BMI calculation on the client-side
-        var bmi = weight / (height * height);
-        bmi = bmi.toFixed(2); // Round the BMI to 2 decimal places
+        // Vérifie si les valeurs saisies sont valides
+        if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+            alert('Veuillez saisir des valeurs numériques positives pour le poids et la taille.');
+            return; // Arrête l'exécution du script en cas de données invalides
+        }
 
-        // Update the result element with the calculated BMI and apply color based on the scale
+        // Effectue le calcul de l'IMC
+        var bmi = weight / (height * height);
+        bmi = bmi.toFixed(2); // Arrondit l'IMC à 2 décimales
+
+        // Effectue la suite du traitement (affichage du résultat, etc.)
         var resultElement = $('#result');
         resultElement.text('Your BMI is: ' + bmi);
-        resultElement.removeClass(); // Remove any previous classes
+        resultElement.removeClass(); // Supprime toutes les classes précédentes
 
         if (bmi < 18.5) {
             resultElement.addClass('underweight');
@@ -28,7 +35,7 @@ $(document).ready(function() {
             resultElement.append('<p class="message">You are obese. It is important to take immediate action for your health. Consider consulting a healthcare professional or nutritionist for personalized advice and support.</p>');
         }
 
-        // Display the paragraph about the importance of BMI
+        // Affiche le paragraphe sur l'importance de l'IMC
         $('#bmi-info').text('BMI is an important indicator of your overall health and can help assess the risk of certain health conditions. However, please note that BMI is a general measure and may not take into account individual variations in body composition and muscle mass.');
     });
 });
